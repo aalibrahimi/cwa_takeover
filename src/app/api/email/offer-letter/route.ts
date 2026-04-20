5430360879a4b2db3358960e4a6625eadd3c39e2
blazehp <blazehunterhp@gmail.com>
2026-04-20 08:46:48 +0200

    Added attachment param and added more comments

--- src/app/api/email/offer-letter/route.ts
+++ src/app/api/email/offer-letter/route.ts
@@ -14,7 +14,6 @@ import { HTTPException } from "hono/http-exception";
 const RESNED_API_KEY = process.env.RESEND_API_KEY;
 
 const app = new Hono().basePath("/api/email");
-const resend = new Resend();
 
 const redis = createClient({
   url: process.env.REDIS_URL,
@@ -29,6 +28,7 @@ if (process.env.NODE_ENV === "development") {
         "Content-Type",
         "Authorization",
         "apikey",
+        "timestamp",
         "Access-Control-Allow-Origin",
       ],
       allowMethods: ["POST", "OPTIONS"],
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "development") {
     "/*",
     cors({
       origin: PROD_CORS_ORIGINS,
-      allowHeaders: ["Content-Type", "Authorization", "apikey"],
+      allowHeaders: ["Content-Type", "Authorization", "apikey", "timestamp"],
       allowMethods: ["POST", "OPTIONS"],
       maxAge: 600,
     }),
@@ -51,51 +51,81 @@ if (process.env.NODE_ENV === "development") {
 app.use(logger());
 
 interface OfferLetterParams {
+  /** Sender details */
   from: {
+    /** Name of Sender */
     name: string;
+    /** Email of Sender */
     email: string;
   };
+  /** Recipient email *( when multiple: **separate by comma** )* */
   to: string;
+  /** Email Subject */
   subject: string;
+  /** Email's body content ( text ) */
   body: string;
+  /** File to with email */
+  attachment: {
+    /** File's name with extension (e.g. my_file.pdf) */
+    filename: string;
+    /** File content sent as text */
+    textContent: string;
+  };
 }
 
 app.post(
   "/offer-letter",
   validator("header", async (value, c) => {
-    const bearerToken = value["bearer"];
+    const bearerToken = value["authorization"];
+    // *This is a custom header created by us
     const timestamp = value["timestamp"];
     if (!redis.isOpen) {
       await redis.connect();
     }
 
-    const cachedToken = await redis.json.get(`bearer:${timestamp}`)
+    const cachedToken = await redis.json.get(`bearer:${timestamp}`);
     if (cachedToken === null) {
       throw new HTTPException(400, {
-        message: "No valid token found"
-      })
+        message: "No valid token found",
+      });
     }
 
     // Check if sent bearer token is valid
     if (cachedToken !== bearerToken) {
       throw new HTTPException(400, {
-        message: "Invalid Bearer Token received"
-      })
+        message: "Invalid Bearer Token received",
+      });
     }
 
     return { bearerToken };
   }),
   async (c) => {
+    if (!RESNED_API_KEY) {
+      console.error("No Resend key found");
+      return c.json({ error: "No Resend Key found" }, 400);
+    }
+    const resend = new Resend(RESNED_API_KEY);
     const body: OfferLetterParams = await c.req.json();
 
     // *Parse emails that are separated by comma
     const emailTo = body.to.split(",");
+
+    // *Possibly need to change method of sending/receiving file
+    const encodedFile = Buffer.from(body.attachment.textContent).toString(
+      "base64",
+    );
 
     const { data, error } = await resend.emails.send({
       from: `${body.from.name} <${body.from.email}>`,
       to: emailTo,
       subject: body.subject,
       react: OfferLetter(body.body),
+      attachments: [
+        {
+          filename: body.attachment.filename,
+          content: encodedFile,
+        },
+      ],
     });
 
     if (error) {
